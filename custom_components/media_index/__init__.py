6661a8f5ea167c24e30ab685f69d241313004882
markaggar <markaggar@hotmail.com>
2026-04-14 15:23:52 -0700

    fix: allow location_tolerance_meters=0 in index_burst_groups service schema (0=GPS disabled)

--- custom_components/media_index/__init__.py
+++ custom_components/media_index/__init__.py
@@ -1912,7 +1912,7 @@ def _register_services(hass: HomeAssistant):
         schema=vol.Schema({
             vol.Optional("folder"): cv.string,
             vol.Optional("time_window_seconds", default=10): vol.All(vol.Coerce(int), vol.Range(min=1, max=300)),
-            vol.Optional("location_tolerance_meters", default=50): vol.All(vol.Coerce(int), vol.Range(min=1, max=1000)),
+            vol.Optional("location_tolerance_meters", default=50): vol.All(vol.Coerce(int), vol.Range(min=0, max=1000)),
             vol.Optional("min_group_size", default=2): vol.All(cv.positive_int, vol.Range(min=2)),
             vol.Optional("overwrite_existing", default=True): cv.boolean,
         }, extra=vol.ALLOW_EXTRA),
